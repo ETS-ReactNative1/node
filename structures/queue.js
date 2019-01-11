2da5d21ce6f0a6627c14d27de9801b09480da678
Juanjo Falcon <jfalcon@fagorhealthcare.com>
2019-01-11 18:04:12 +0100

    ADD: structure Queue

---
+++ structures/queue.js
@@ -0,0 +1,37 @@
+//queue
+
+function createQueue() {
+    const queue = []
+
+    return {
+        enqueue(item) {
+            queue.unshift(item)
+        },
+        dequeue() {
+            return queue.pop()
+        },
+        peek() {
+            return queue[queue.length-1]
+        },
+        get length() {
+            return queue.length
+        },
+        isEmpty() {
+            return queue.length === 0
+        }
+
+    }
+}
+
+const q = createQueue()
+console.log(q.isEmpty())
+
+q.enqueue('Make a lesson')
+q.enqueue('Help others learn')
+q.enqueue('Be happy')
+
+q.dequeue()
+q.dequeue()
+q.dequeue()
+console.log(q.peek())
+console.log(q.isEmpty())
