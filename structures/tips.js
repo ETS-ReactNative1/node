6c583f45603c23ad8a27ec641cbceed4c07c0bab
Juanjo Falcon <jfalcon@fagorhealthcare.com>
2019-02-04 14:19:19 +0100

    update tips.js - include write, read js objets in json file

--- structures/tips.js
+++ structures/tips.js
@@ -1,3 +1,28 @@
+//Tip: Escribir objeto a JSON 
+//https://es.quora.com/C%C3%B3mo-hago-un-archivo-JSON-con-JavaScript
+//writeFile(), recibe 4 parametros, el path o ruta del archivo con nombre y extension,
+//los datos a guardar en dicho archivo, la codificacion de caracteres a usar y como todo en javascript un callback.
+
+'use strict';
+const fs = require('fs');
+
+let student = {  
+    name: 'Mike',
+    age: 23, 
+    gender: 'Male',
+    department: 'English',
+    car: 'Honda',
+    languages: ["JavaScript", "Python", "C++"],
+    "best friends": {"name": "Marco", "age":15}
+};
+
+let data = JSON.stringify(student);  
+fs.writeFileSync('student.json', data); 
+
+data = fs.readFileSync('student.json');  
+student = JSON.parse(data);  
+console.log(student);  
+
 //Tip: Consola 
 //https://www.genbeta.com/desarrollo/exprimiendo-javascript-y-su-consola
 //La consola JavaScript soporta varios niveles para organizar (log, info, warn, error, debug) 
