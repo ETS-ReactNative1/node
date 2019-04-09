3e7c5b2d1d15f613f5daf6b985866e2bd7f40fcf
Juanjo Falcon <jfalcon@fagorhealthcare.com>
2019-04-09 17:13:09 +0200

    ADD: algorithms

---
+++ structures/algorithms.js
@@ -0,0 +1,5 @@
+//The Ultimate Guide to JavaScript Algorithms
+//https://scotch.io/courses/the-ultimate-guide-to-javascript-algorithms
+
+//Range Sum
+//https://scotch.io/courses/the-ultimate-guide-to-javascript-algorithms/range-sum
